9933420e44527a0e4d702e21df2eec39f910f5d1
jrbigmon <juniorterriaga0@gmail.com>
2023-02-28 20:52:33 -0300

    feat: changed log routes

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -8,29 +8,36 @@ import { getBrowser } from 'src/utils/browser-detect';
 export class LoggerMiddleware implements NestMiddleware {
   constructor(private readonly logsService: LogsService) {}
 
-  private readonly logger = new Logger('Users');
+  private readonly logger = new Logger('HTTP');
 
   async use(req: Request, res: Response, next: () => void) {
     const { method, originalUrl, body, ip, headers } = req;
 
-    const deviceString = headers['user-agent'];
-
-    const deviceObject = getBrowser(deviceString);
-
-    const bodyIsEmpty = Object.keys(body).length === 0;
+    const deviceObject = getBrowser(headers['user-agent']);
 
     res.on('finish', async () => {
       const { statusCode, statusMessage } = res;
 
       if (body && body?.hasOwnProperty('password')) delete body['password'];
 
-      const content = `${method} ${originalUrl} ${statusCode} ${statusMessage} - ${ip} ${
-        !bodyIsEmpty ? JSON.stringify(body) : ''
-      } ${deviceString}`;
+      const content = {
+        method,
+        originalUrl,
+        statusCode,
+        statusMessage,
+        ip,
+        body,
+        device: deviceObject,
+      };
 
-      if (statusCode >= 100 && statusCode <= 399) this.logger.log(content);
-      if (statusCode >= 400 && statusCode <= 499) this.logger.warn(content);
-      if (statusCode >= 500 && statusCode <= 599) this.logger.error(content);
+      if (statusCode >= 100 && statusCode <= 399)
+        this.logger.log(JSON.stringify(content));
+
+      if (statusCode >= 400 && statusCode <= 499)
+        this.logger.warn(JSON.stringify(content));
+
+      if (statusCode >= 500 && statusCode <= 599)
+        this.logger.error(JSON.stringify(content));
 
       await this.logsService.create({
         ...deviceObject,
