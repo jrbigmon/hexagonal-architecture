3b4488a030f7008fc82aef44f47b86149f243be4
jrbigmon <juniorterriaga0@gmail.com>
2023-03-05 17:26:01 -0300

    fix: fixed name of property

--- src/locations/dto/location-create.dto.ts
+++ src/locations/dto/location-create.dto.ts
@@ -2,7 +2,7 @@ import { IsString } from 'class-validator';
 
 export class LocationCreateDto {
   @IsString({ message: 'Adress Line is string' })
-  adressLine1: string;
+  addressLine1: string;
   @IsString({ message: 'Code is string' })
   code: string;
   @IsString({ message: 'address number is string' })
