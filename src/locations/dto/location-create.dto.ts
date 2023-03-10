b48b053e52715cdaebd30a377cdb1031992eb40a
jrbigmon <juniorterriaga0@gmail.com>
2023-03-10 19:51:58 -0300

    style: adjusted text in location

--- src/locations/dto/location-create.dto.ts
+++ src/locations/dto/location-create.dto.ts
@@ -1,10 +1,10 @@
 import { IsString } from 'class-validator';
 
 export class LocationCreateDto {
-  @IsString({ message: 'Adress Line is string' })
+  @IsString({ message: 'Address Line is string' })
   addressLine1: string;
   @IsString({ message: 'Code is string' })
   code: string;
   @IsString({ message: 'address number is string' })
-  adressNumber: string;
+  addressNumber: string;
 }
